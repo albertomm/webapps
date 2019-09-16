ce34a55a6e1ee0df1753d40032431bc231afdff0
Alberto <albertomm@gmail.com>
2019-09-16 23:21:25 +0200

    More apps.

--- build.sh
+++ build.sh
@@ -15,3 +15,6 @@ $nativefier --maximize --single-instance --user-agent="$useragent" --name="skype
 $nativefier --maximize --single-instance --user-agent="$useragent" --name="netflix"  --icon="icons/icon-netflix.png" "https://www.netflix.com" "$dest"
 $nativefier --maximize --single-instance --user-agent="$useragent" --name="twitch"  --icon="icons/icon-twitch.png" "https://www.twitch.tv" "$dest"
 $nativefier --maximize --single-instance --user-agent="$useragent" --name="keep"  --icon="icons/icon-keep.png" "https://keep.google.com" "$dest"
+$nativefier --maximize --single-instance --user-agent="$useragent" --name="discord" --icon="icons/icon-discord.png" "https://discordapp.com/channels/@me" "$dest"
+$nativefier --maximize --single-instance --user-agent="$useragent" --name="slack" --icon="icons/icon-slack.png" "https://slack.com/" "$dest"
+$nativefier --maximize --single-instance --user-agent="$useragent" --name="spotify" "https://open.spotify.com/" "$dest"
